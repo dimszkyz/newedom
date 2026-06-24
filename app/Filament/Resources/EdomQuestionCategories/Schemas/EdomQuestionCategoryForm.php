bcf9e8f8be465ab37987b7010b6c895aa5c9993e
Dimas Rizky <schooldims@gmail.com>
2026-06-24 14:44:47 +0700

    Add question category form

---
+++ app/Filament/Resources/EdomQuestionCategories/Schemas/EdomQuestionCategoryForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\EdomQuestionCategories\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class EdomQuestionCategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema->components([
+            Select::make('edom_setting_id')
+                ->label('EDOM')
+                ->relationship('edom', 'name')
+                ->searchable()
+                ->preload()
+                ->required(),
+
+            TextInput::make('name')
+                ->label('Nama Kategori')
+                ->required()
+                ->maxLength(255),
+        ]);
+    }
+}
