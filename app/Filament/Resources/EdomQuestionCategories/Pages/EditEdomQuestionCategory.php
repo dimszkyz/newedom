3d496b1542a8a73c2c3b883948d536c6f9e2252a
Dimas Rizky <schooldims@gmail.com>
2026-06-26 13:08:22 +0700

    Add EdomQuestionCategory edit page

---
+++ app/Filament/Resources/EdomQuestionCategories/Pages/EditEdomQuestionCategory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\EdomQuestionCategories\Pages;
+
+use App\Filament\Resources\EdomQuestionCategories\EdomQuestionCategoryResource;
+use App\Filament\Resources\Edoms\EdomResource;
+use Filament\Actions\DeleteAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditEdomQuestionCategory extends EditRecord
+{
+    protected static string $resource = EdomQuestionCategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make()
+                ->visible(fn () => $this->record->edom?->isDraft()),
+        ];
+    }
+
+    public function getBreadcrumbs(): array
+    {
+        $edom = $this->record->edom;
+
+        if (! $edom) {
+            return [
+                EdomQuestionCategoryResource::getUrl() => 'Kategori Pertanyaan EDOM',
+                '' => $this->record->name,
+            ];
+        }
+
+        return [
+            EdomResource::getUrl() => 'Kelola EDOM',
+            EdomResource::getUrl('edit', [
+                'record' => $edom,
+            ]) => $edom->name,
+            '' => $this->record->name,
+        ];
+    }
+}
