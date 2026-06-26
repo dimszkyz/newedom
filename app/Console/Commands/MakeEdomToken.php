bc51f1db46135aeda3f54e6c69bec6ff03812e6f
Dimas Rizky <schooldims@gmail.com>
2026-06-26 13:10:00 +0700

    Allow default mahasiswa id for EDOM token command

--- app/Console/Commands/MakeEdomToken.php
+++ app/Console/Commands/MakeEdomToken.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class MakeEdomToken extends Command
 {
-    protected $signature = 'edom:make-token {idmahasiswa} {idtahunajaran} {idsemester} {--ttl=3600} {--return-url=}';
+    protected $signature = 'edom:make-token {first} {second?} {third?} {--idmahasiswa=} {--ttl=3600} {--return-url=}';
 
     protected $description = 'Mint a siakad-style EDOM handoff token for testing /enter';
 
@@ -20,13 +20,21 @@ class MakeEdomToken extends Command
             return self::FAILURE;
         }
 
+        [$siakadIdMahasiswa, $siakadIdTahunAjaran, $siakadIdSemester] = $this->resolveArguments();
+
+        if ($siakadIdTahunAjaran <= 0 || $siakadIdSemester <= 0 || $siakadIdMahasiswa <= 0) {
+            $this->error('Invalid arguments. Use: php artisan edom:make-token {idtahunajaran} {idsemester} or php artisan edom:make-token {idmahasiswa} {idtahunajaran} {idsemester}.');
+
+            return self::FAILURE;
+        }
+
         $returnUrl = $this->option('return-url')
             ?: rtrim((string) config('edom.siakad_fallback_url', config('app.url')), '/').'/edom';
 
         $payload = [
-            'siakad_idmahasiswa' => (int) $this->argument('idmahasiswa'),
-            'siakad_idtahunajaran' => (int) $this->argument('idtahunajaran'),
-            'siakad_idsemester' => (int) $this->argument('idsemester'),
+            'siakad_idmahasiswa' => $siakadIdMahasiswa,
+            'siakad_idtahunajaran' => $siakadIdTahunAjaran,
+            'siakad_idsemester' => $siakadIdSemester,
             'return_url' => $returnUrl,
             'exp' => time() + (int) $this->option('ttl'),
         ];
@@ -36,9 +44,33 @@ class MakeEdomToken extends Command
         $token = $b64.'.'.$signature;
         $url = rtrim((string) config('app.url'), '/').'/enter?token='.$token;
 
+        $this->line('siakad_idmahasiswa: '.$siakadIdMahasiswa);
+        $this->line('siakad_idtahunajaran: '.$siakadIdTahunAjaran);
+        $this->line('siakad_idsemester: '.$siakadIdSemester);
         $this->line('token: '.$token);
         $this->line('url:   '.$url);
 
         return self::SUCCESS;
+    }
+
+    /**
+     * Supports both forms:
+     * - php artisan edom:make-token 2025 2
+     * - php artisan edom:make-token 18273 2025 2
+     */
+    private function resolveArguments(): array
+    {
+        $first = $this->argument('first');
+        $second = $this->argument('second');
+        $third = $this->argument('third');
+
+        if ($third !== null) {
+            return [(int) $first, (int) $second, (int) $third];
+        }
+
+        $defaultMahasiswa = $this->option('idmahasiswa')
+            ?: config('edom.test_siakad_idmahasiswa', 18273);
+
+        return [(int) $defaultMahasiswa, (int) $first, (int) $second];
     }
 }
