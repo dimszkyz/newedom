9670f923f31390aeb666cdf76aa6ea23495ec0a2
Dimas Rizky <schooldims@gmail.com>
2026-06-29 14:13:43 +0700

    Restore Edom period list page class name

--- app/Filament/Resources/EdomPeriods/Pages/ListEdomPeriods.php
+++ app/Filament/Resources/EdomPeriods/Pages/ListEdomPeriods.php
@@ -4,16 +4,14 @@ namespace App\Filament\Resources\EdomPeriods\Pages;
 
 use App\Filament\Resources\EdomPeriods\EdomPeriodResource;
 use Filament\Actions\CreateAction;
-use Filament\Resources\Pages\ManageRecords;
+use Filament\Resources\Pages\ListRecords;
 
-class ManageEdomPeriods extends ManageRecords
+class ListEdomPeriods extends ListRecords
 {
     protected static string $resource = EdomPeriodResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [
-            CreateAction::make(),
-        ];
+        return [CreateAction::make()];
     }
 }
