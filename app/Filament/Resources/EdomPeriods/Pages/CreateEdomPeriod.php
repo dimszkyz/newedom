ed5ad7465eea494b35d19b0b133b77d6cdb55a35
Dimas Rizky <schooldims@gmail.com>
2026-06-29 14:13:50 +0700

    Reuse create page file as modal manage page

--- app/Filament/Resources/EdomPeriods/Pages/CreateEdomPeriod.php
+++ app/Filament/Resources/EdomPeriods/Pages/CreateEdomPeriod.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\EdomPeriods\Pages;
 
 use App\Filament\Resources\EdomPeriods\EdomPeriodResource;
-use Filament\Resources\Pages\CreateRecord;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ManageRecords;
 
-class CreateEdomPeriod extends CreateRecord
+class ManageEdomPeriods extends ManageRecords
 {
     protected static string $resource = EdomPeriodResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
