10b13ef4e2b552eaccabd02bc8042bfec9ae4801
Dimas Rizky <schooldims@gmail.com>
2026-07-01 10:12:28 +0700

    Show aggregated results in existing EdomResponse page

--- app/Filament/Resources/EdomResponses/Pages/ListEdomResponses.php
+++ app/Filament/Resources/EdomResponses/Pages/ListEdomResponses.php
@@ -3,9 +3,21 @@
 namespace App\Filament\Resources\EdomResponses\Pages;
 
 use App\Filament\Resources\EdomResponses\EdomResponseResource;
+use App\Services\Edom\EdomResultAggregator;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Collection;
 
 class ListEdomResponses extends ListRecords
 {
     protected static string $resource = EdomResponseResource::class;
+
+    protected string $view = 'filament.resources.edom-responses.pages.list-edom-responses';
+
+    /**
+     * @return Collection<string, Collection<int, array<string, mixed>>>
+     */
+    public function getGroupedResults(): Collection
+    {
+        return app(EdomResultAggregator::class)->groupedSummaries();
+    }
 }
