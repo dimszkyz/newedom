356e5416e15227d93982b93e32e2ad9dceb39f08
Dimas Rizky <schooldims@gmail.com>
2026-07-02 14:17:36 +0700

    Add EDOM report program studi list page

---
+++ app/Filament/Resources/EdomReports/Pages/ListEdomReports.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\EdomReports\Pages;
+
+use App\Filament\Resources\EdomReports\EdomReportResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListEdomReports extends ListRecords
+{
+    protected static string $resource = EdomReportResource::class;
+}
