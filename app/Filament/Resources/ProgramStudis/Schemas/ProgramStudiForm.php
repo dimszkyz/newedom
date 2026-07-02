784106947727ed53540da75597152be55a3e11bd
Dimas Rizky <schooldims@gmail.com>
2026-07-02 10:41:54 +0700

    Show detailed Program Studi fields

--- app/Filament/Resources/ProgramStudis/Schemas/ProgramStudiForm.php
+++ app/Filament/Resources/ProgramStudis/Schemas/ProgramStudiForm.php
@@ -11,11 +11,43 @@ class ProgramStudiForm
     {
         return $schema->components([
             TextInput::make('id_unw_program_studi')
-                ->label('ID Program Studi API UNW')
+                ->label('id_unw_program_studi')
+                ->disabled(),
+
+            TextInput::make('display_name')
+                ->label('Tampilan Admin')
                 ->disabled(),
 
             TextInput::make('nama')
                 ->label('Nama Program Studi')
+                ->disabled(),
+
+            TextInput::make('jenjang')
+                ->label('Jenjang')
+                ->disabled(),
+
+            TextInput::make('jenjang_nama_singkat')
+                ->label('Jenjang Singkat')
+                ->disabled(),
+
+            TextInput::make('slug')
+                ->label('Slug')
+                ->disabled(),
+
+            TextInput::make('page_slug')
+                ->label('Page Slug')
+                ->disabled(),
+
+            TextInput::make('id_unw_fakultas')
+                ->label('ID Fakultas UNW')
+                ->disabled(),
+
+            TextInput::make('nama_fakultas')
+                ->label('Nama Fakultas')
+                ->disabled(),
+
+            TextInput::make('page_slug_fakultas')
+                ->label('Page Slug Fakultas')
                 ->disabled(),
         ]);
     }
