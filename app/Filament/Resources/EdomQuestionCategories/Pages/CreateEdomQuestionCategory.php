b2cdedf0c8ed108ae2932ee46ac7099267c18dd3
Dimas Rizky <schooldims@gmail.com>
2026-07-08 09:01:15 +0700

    feat(edom): add back button to create category

--- app/Filament/Resources/EdomQuestionCategories/Pages/CreateEdomQuestionCategory.php
+++ app/Filament/Resources/EdomQuestionCategories/Pages/CreateEdomQuestionCategory.php
@@ -3,9 +3,22 @@
 namespace App\Filament\Resources\EdomQuestionCategories\Pages;
 
 use App\Filament\Resources\EdomQuestionCategories\EdomQuestionCategoryResource;
+use App\Filament\Resources\EdomSettings\EdomSettingsResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEdomQuestionCategory extends CreateRecord
 {
     protected static string $resource = EdomQuestionCategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('back')
+                ->label('Kembali')
+                ->icon('heroicon-o-arrow-left')
+                ->color('gray')
+                ->url(EdomSettingsResource::getUrl('index')),
+        ];
+    }
 }
