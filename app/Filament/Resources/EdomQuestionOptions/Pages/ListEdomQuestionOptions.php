0804848983ceec7cba28a8725ae772bb94924689
Dimas Rizky <schooldims@gmail.com>
2026-07-08 09:01:33 +0700

    feat(edom): add back button to option list

--- app/Filament/Resources/EdomQuestionOptions/Pages/ListEdomQuestionOptions.php
+++ app/Filament/Resources/EdomQuestionOptions/Pages/ListEdomQuestionOptions.php
@@ -3,9 +3,22 @@
 namespace App\Filament\Resources\EdomQuestionOptions\Pages;
 
 use App\Filament\Resources\EdomQuestionOptions\EdomQuestionOptionResource;
+use App\Filament\Resources\EdomSettings\EdomSettingsResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ListRecords;
 
 class ListEdomQuestionOptions extends ListRecords
 {
     protected static string $resource = EdomQuestionOptionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('back')
+                ->label('Kembali')
+                ->icon('heroicon-o-arrow-left')
+                ->color('gray')
+                ->url(EdomSettingsResource::getUrl('index')),
+        ];
+    }
 }
