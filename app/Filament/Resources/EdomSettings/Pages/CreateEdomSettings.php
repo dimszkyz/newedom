00f504c9fd2098118d54fad2bfa610cb3407b79a
Dimas Rizky <schooldims@gmail.com>
2026-07-08 09:00:46 +0700

    feat(edom): add back button to create settings

--- app/Filament/Resources/EdomSettings/Pages/CreateEdomSettings.php
+++ app/Filament/Resources/EdomSettings/Pages/CreateEdomSettings.php
@@ -3,9 +3,21 @@
 namespace App\Filament\Resources\EdomSettings\Pages;
 
 use App\Filament\Resources\EdomSettings\EdomSettingsResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEdomSettings extends CreateRecord
 {
     protected static string $resource = EdomSettingsResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('back')
+                ->label('Kembali')
+                ->icon('heroicon-o-arrow-left')
+                ->color('gray')
+                ->url(EdomSettingsResource::getUrl('index')),
+        ];
+    }
 }
