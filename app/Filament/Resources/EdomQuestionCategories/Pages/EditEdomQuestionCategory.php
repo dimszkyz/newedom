265f1609de62dce722f44e9e5d2ac927f4d8b373
Dimas Rizky <schooldims@gmail.com>
2026-07-08 09:00:55 +0700

    feat(edom): add back button to edit category

--- app/Filament/Resources/EdomQuestionCategories/Pages/EditEdomQuestionCategory.php
+++ app/Filament/Resources/EdomQuestionCategories/Pages/EditEdomQuestionCategory.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\EdomQuestionCategories\Pages;
 
 use App\Filament\Resources\EdomQuestionCategories\EdomQuestionCategoryResource;
+use App\Filament\Resources\EdomSettings\EdomSettingsResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
@@ -13,6 +15,13 @@ class EditEdomQuestionCategory extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('back')
+                ->label('Kembali')
+                ->icon('heroicon-o-arrow-left')
+                ->color('gray')
+                ->url(fn (): string => $this->record->edom_setting_id
+                    ? EdomSettingsResource::getUrl('edit', ['record' => $this->record->edom_setting_id])
+                    : EdomSettingsResource::getUrl('index')),
             DeleteAction::make()
                 ->visible(fn (): bool => $this->record->edomSettings?->isDraft() ?? false),
         ];
